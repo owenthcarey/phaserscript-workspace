01a93dca31d43ae85293017f547012dad5aa1574
Owen Carey <owenthcarey@gmail.com>
2023-05-25 11:16:40 -0700

    Update games component

--- apps/web-phaserscript/src/app/features/games/components/games/games.component.ts
+++ apps/web-phaserscript/src/app/features/games/components/games/games.component.ts
@@ -5,8 +5,21 @@ import { BaseComponent } from '@phaserscript-workspace/xplat/core';
 @Component({
   selector: 'phaserscript-workspace-games',
   templateUrl: 'games.component.html',
+  styleUrls: ['games.component.scss'],
 })
 export class GamesComponent extends BaseComponent {
+  games = [
+    {
+      title: 'Pinball',
+      subtitle: 'Arcade Game',
+      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      description:
+        'A fun, fast-paced pinball game. Test your skills and beat your high score!',
+      route: '/games/pinball',
+    },
+    // Add more game objects here later...
+  ];
+
   constructor() {
     super();
   }
--- apps/web-phaserscript/src/app/features/games/games.module.ts
+++ apps/web-phaserscript/src/app/features/games/games.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { GAMES_COMPONENTS } from './components';
 import { GamesRoutingModule } from './games-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [SharedModule, GamesRoutingModule],
+  imports: [SharedModule, GamesRoutingModule, MatCardModule, MatButtonModule],
   declarations: [...GAMES_COMPONENTS],
   exports: [...GAMES_COMPONENTS],
 })
