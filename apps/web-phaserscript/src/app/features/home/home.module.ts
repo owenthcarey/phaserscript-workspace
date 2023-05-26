c6c82ea1a1b64bd55fc08eff0ae5f61073e7caf1
Owen Carey <owenthcarey@gmail.com>
2023-05-25 17:55:20 -0700

    Update home component

--- apps/web-phaserscript/src/app/features/home/home.module.ts
+++ apps/web-phaserscript/src/app/features/home/home.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 import { HOME_COMPONENTS, HomeComponent } from './components';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 export const routes: Routes = [
   {
@@ -12,7 +14,12 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild(routes)],
+  imports: [
+    SharedModule,
+    RouterModule.forChild(routes),
+    MatButtonModule,
+    MatCardModule,
+  ],
   declarations: [...HOME_COMPONENTS],
   exports: [...HOME_COMPONENTS],
 })
