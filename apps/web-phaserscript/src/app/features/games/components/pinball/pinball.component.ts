3f081c15e14ed459188b6408657df1749949879f
owenthcarey <owenthcarey@gmail.com>
2023-05-29 09:54:49 -0700

    Update pinball component

--- apps/web-phaserscript/src/app/features/games/components/pinball/pinball.component.ts
+++ apps/web-phaserscript/src/app/features/games/components/pinball/pinball.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { PinballScene } from './pinball.scene';
 import Phaser from 'phaser';
 
@@ -10,16 +17,17 @@ import { BaseComponent } from '@phaserscript-workspace/xplat/core';
 })
 export class PinballComponent
   extends BaseComponent
-  implements OnInit, OnDestroy
+  implements AfterViewInit, OnInit, OnDestroy
 {
+  @ViewChild('gameContainer') gameContainer?: ElementRef;
   phaserGame?: Phaser.Game;
   config?: Phaser.Types.Core.GameConfig;
 
   ngOnInit() {
     this.config = {
       type: Phaser.AUTO,
-      height: 800,
-      width: 600,
+      // height: 800,
+      // width: 600,
       scene: [PinballScene],
       parent: 'gameContainer',
       physics: {
@@ -28,12 +36,35 @@ export class PinballComponent
           debug: true,
         },
       },
+      scale: {
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+      },
     };
     this.phaserGame = new Phaser.Game(this.config);
   }
 
+  ngAfterViewInit() {
+    if (this.gameContainer) {
+      this.config!.width = this.gameContainer.nativeElement.clientWidth;
+      this.config!.height = this.gameContainer.nativeElement.clientHeight;
+      this.phaserGame = new Phaser.Game(this.config);
+      window.addEventListener('resize', this.resizeGame);
+    }
+  }
+
+  resizeGame = () => {
+    if (this.gameContainer && this.phaserGame) {
+      this.phaserGame.scale.resize(
+        this.gameContainer.nativeElement.clientWidth,
+        this.gameContainer.nativeElement.clientHeight
+      );
+    }
+  };
+
   override ngOnDestroy() {
     super.ngOnDestroy();
     this.phaserGame?.destroy(true);
+    window.removeEventListener('resize', this.resizeGame);
   }
 }
--- apps/web-phaserscript/src/app/features/games/components/pinball/pinball.scene.ts
+++ apps/web-phaserscript/src/app/features/games/components/pinball/pinball.scene.ts
@@ -1,4 +1,5 @@
 import * as Phaser from 'phaser';
+import { BodyType } from 'matter';
 
 export class PinballScene extends Phaser.Scene {
   ball?: Phaser.Physics.Matter.Image;
@@ -11,9 +12,12 @@ export class PinballScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('ball', 'assets/ball.png');
-    this.load.image('flipper', 'assets/flipper.png');
-    // Load other assets like bumpers and pins
+    this.load.image('ball', 'assets/pinball/ball.png');
+    this.load.image('flipper', 'assets/pinball/flipper.png');
+    this.load.image('bumper', 'assets/pinball/bumper.png');
+    this.load.image('slingshot', 'assets/pinball/slingshot.png');
+    this.load.image('square', 'assets/pinball/square.png');
+    this.load.image('wall', 'assets/pinball/wall.png');
   }
 
   create() {
@@ -23,34 +27,80 @@ export class PinballScene extends Phaser.Scene {
       this.cameras.main.width,
       this.cameras.main.height
     );
+    // this.add.image(400, 500, 'wall'); // Wall at the center
+    this.matter.add.image(200, 300, 'slingshot').setStatic(true).setAngle(180); // Slingshot at the center-top
+    this.matter.add.image(600, 300, 'slingshot').setStatic(true); // Slingshot at the center-top
+    // this.add.image(400, 700, 'square'); // Square at the center-bottom
+    this.matter.add.image(200, 500, 'bumper').setStatic(true); // Bumper on the left
+    this.matter.add.image(600, 500, 'bumper').setStatic(true); // Bumper on the right
+
+    // Top wall
+    this.matter.add
+      .image(this.cameras.main.width / 2, 100, 'wall')
+      .setStatic(true);
+
+    // Bottom wall
+    this.matter.add
+      .image(
+        this.cameras.main.width / 2,
+        this.cameras.main.height - 100,
+        'wall'
+      )
+      .setStatic(true);
+
+    // Left wall, rotated 90 degrees
+    this.matter.add
+      .image(100, this.cameras.main.height / 2, 'wall')
+      .setAngle(90)
+      .setStatic(true);
+
+    // Right wall, rotated 90 degrees
+    this.matter.add
+      .image(
+        this.cameras.main.width - 100,
+        this.cameras.main.height / 2,
+        'wall'
+      )
+      .setAngle(90)
+      .setStatic(true);
 
     this.ball = this.matter.add.image(400, 500, 'ball');
-    this.ball.setCircle(8); // Assumes the ball is a perfect circle
+    this.ball.setCircle(8);
     this.ball.setBounce(1);
     this.ball.setFriction(0, 0, 0);
 
     this.leftFlipper = this.matter.add.image(300, 700, 'flipper');
     this.rightFlipper = this.matter.add.image(500, 700, 'flipper');
-    // Add constraints (joints) to the flippers so they can rotate
+    // Depending on the shape and orientation of your flipper image,
+    // you might need to rotate the flippers to position them correctly.
+    // For example:
+    // this.leftFlipper.setAngle(-30);
+    // this.rightFlipper.setAngle(30);
 
-    // Create and position other elements like bumpers and pins
+    // Apply constraints to flippers
+    // this.matter.add.constraint(
+    //   this.leftFlipper.body as BodyType,
+    //   this.rightFlipper.body as BodyType,
+    //   80,
+    //   0.8
+    // );
+    // this.matter.add.mouseSpring({});
 
     this.cursors = this.input.keyboard?.createCursorKeys();
   }
 
   override update() {
     if (this.cursors?.left.isDown) {
-      // Apply a force or torque to leftFlipper
-    }
-    if (this.cursors?.right.isDown) {
-      // Apply a force or torque to rightFlipper
-    }
-    if (this.cursors?.up.isDown) {
-      // Launch the ball if it's not already in play
+      this.leftFlipper?.setAngularVelocity(-0.15);
+    } else if (this.cursors?.right.isDown) {
+      this.rightFlipper?.setAngularVelocity(0.15);
+    } else {
+      this.leftFlipper?.setAngularVelocity(0);
+      this.rightFlipper?.setAngularVelocity(0);
     }
     if (this.ball!.y > this.cameras.main.height) {
-      // Reset the ball if it fell off the bottom
+      this.ball?.setPosition(400, 500);
+      this.ball?.setVelocity(0);
     }
-    // Handle other gameplay mechanics like scoring
   }
 }
