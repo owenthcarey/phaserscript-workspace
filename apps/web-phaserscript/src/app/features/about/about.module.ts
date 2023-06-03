3f22fa8b03cefde009dcf867e603b03ae966082f
Owen Carey <owenthcarey@gmail.com>
2023-06-02 17:16:10 -0700

    Add about-table component

--- apps/web-phaserscript/src/app/features/about/about.module.ts
+++ apps/web-phaserscript/src/app/features/about/about.module.ts
@@ -3,9 +3,18 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { ABOUT_COMPONENTS } from './components';
 import { AboutRoutingModule } from './about-routing.module';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [SharedModule, AboutRoutingModule],
+  imports: [
+    SharedModule,
+    AboutRoutingModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   declarations: [...ABOUT_COMPONENTS],
   exports: [...ABOUT_COMPONENTS],
 })
--- apps/web-phaserscript/src/app/features/about/components/index.ts
+++ apps/web-phaserscript/src/app/features/about/components/index.ts
@@ -1,5 +1,7 @@
 import { AboutComponent } from './about/about.component';
+import { AboutTableComponent } from './about-table/about-table.component';
 
-export const ABOUT_COMPONENTS = [AboutComponent];
+export const ABOUT_COMPONENTS = [AboutComponent, AboutTableComponent];
 
 export * from './about/about.component';
+export * from './about-table/about-table.component';
