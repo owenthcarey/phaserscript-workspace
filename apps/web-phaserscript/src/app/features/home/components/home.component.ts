570fe71af736a397bbba27ad37fafe6cc92822be
owenthcarey <owenthcarey@gmail.com>
2023-11-11 20:29:49 -0800

    Add Tailwind

--- apps/web-phaserscript/src/app/features/home/components/home.component.ts
+++ apps/web-phaserscript/src/app/features/home/components/home.component.ts
@@ -5,6 +5,6 @@ import { BaseComponent } from '@phaserscript-workspace/xplat/core';
 @Component({
   selector: 'phaserscript-workspace-home',
   templateUrl: 'home.component.html',
-  styleUrls: ['home.component.scss'],
+  // styleUrls: ['home.component.scss'],
 })
 export class HomeComponent extends BaseComponent {}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['apps/web-phaserscript/src/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
