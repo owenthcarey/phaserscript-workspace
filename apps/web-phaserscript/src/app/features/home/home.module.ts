9695ceaa74e5883ff9373e59028bfbe29cd1a1bd
owenthcarey <owenthcarey@gmail.com>
2023-11-21 21:08:41 -0800

    Update home component

--- apps/web-phaserscript/src/app/features/home/home.module.ts
+++ apps/web-phaserscript/src/app/features/home/home.module.ts
@@ -5,6 +5,7 @@ import { SharedModule } from '../shared/shared.module';
 import { HOME_COMPONENTS, HomeComponent } from './components';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 export const routes: Routes = [
   {
@@ -19,6 +20,7 @@ export const routes: Routes = [
     RouterModule.forChild(routes),
     MatButtonModule,
     MatCardModule,
+    MatGridListModule,
   ],
   declarations: [...HOME_COMPONENTS],
   exports: [...HOME_COMPONENTS],
