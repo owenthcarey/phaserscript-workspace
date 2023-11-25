ed0478306d4abcc5840472df7d29a89c99454283
owenthcarey <owenthcarey@gmail.com>
2023-11-25 12:57:45 -0800

    Update pinball component

--- apps/web-phaserscript/src/app/features/games/components/pinball/pinball.component.ts
+++ apps/web-phaserscript/src/app/features/games/components/pinball/pinball.component.ts
@@ -3,7 +3,6 @@ import {
   Component,
   ElementRef,
   OnDestroy,
-  OnInit,
   ViewChild,
 } from '@angular/core';
 import { PinballScene } from './pinball.scene';
@@ -18,39 +17,33 @@ import { BaseComponent } from '@phaserscript/xplat/core';
 })
 export class PinballComponent
   extends BaseComponent
-  implements AfterViewInit, OnInit, OnDestroy
+  implements AfterViewInit, OnDestroy
 {
   @ViewChild('gameContainer') gameContainer?: ElementRef;
   phaserGame?: Phaser.Game;
   config?: Phaser.Types.Core.GameConfig;
 
-  ngOnInit() {
-    this.config = {
-      type: Phaser.AUTO,
-      // height: 800,
-      // width: 600,
-      scene: [PinballScene],
-      parent: 'gameContainer',
-      physics: {
-        default: 'matter',
-        matter: {
-          debug: true,
-        },
-      },
-      scale: {
-        mode: Phaser.Scale.RESIZE,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-      },
-      autoRound: false, // prevent rounding of pixels, which can cause display issues
-    };
-    // this.phaserGame = new Phaser.Game(this.config);
-  }
-
   ngAfterViewInit() {
     if (this.gameContainer) {
-      this.config!.width = this.gameContainer.nativeElement.clientWidth;
-      this.config!.height = this.gameContainer.nativeElement.clientHeight;
-      // Create the Phaser game after getting the actual game container's dimensions
+      this.config = {
+        type: Phaser.AUTO,
+        height: this.gameContainer.nativeElement.clientHeight,
+        width: this.gameContainer.nativeElement.clientWidth,
+        scene: [PinballScene],
+        parent: 'gameContainer',
+        physics: {
+          default: 'matter',
+          matter: {
+            debug: true,
+          },
+        },
+        scale: {
+          mode: Phaser.Scale.RESIZE,
+          autoCenter: Phaser.Scale.CENTER_BOTH,
+        },
+        autoRound: false, // Prevent rounding of pixels
+      };
+      // Create the Phaser game after getting the game container's dimensions
       this.phaserGame = new Phaser.Game(this.config);
       window.addEventListener('resize', this.resizeGame);
     }
