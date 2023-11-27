995ed060da5dab57fd08764a3a4ef22efa9dfbbb
owenthcarey <owenthcarey@gmail.com>
2023-11-26 19:32:16 -0800

    Update contact and home component

--- apps/web-phaserscript/src/app/features/contact/contact.component.ts
+++ apps/web-phaserscript/src/app/features/contact/contact.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'phaserscript-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
   standalone: true,
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule],
 })
 export class ContactComponent {
   // Component logic goes here
