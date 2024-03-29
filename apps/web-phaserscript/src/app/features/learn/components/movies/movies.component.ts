5fe8d37d8bf57217ef20624965e4504da7de8329
owenthcarey <owenthcarey@gmail.com>
2024-03-29 13:05:11 -0700

    feat: update movies component and service

--- apps/web-phaserscript/src/app/features/learn/components/movies/movies.component.ts
+++ apps/web-phaserscript/src/app/features/learn/components/movies/movies.component.ts
@@ -16,6 +16,22 @@ export class MoviesComponent implements OnInit {
     this.movieService.getUpcomingMovies().subscribe((response: any) => {
       this.movies = response.results;
       // Here you might need to loop through movies to fetch additional details like trailers
+      // for (const movie of this.movies) {
+      //   console.log(`${movie.title} trailer:`, this.movieService.getMovieTrailers(movie.id));
+      // }
+      // for (const movie of this.movies) {
+      //   this.movieService.getMovieTrailers(movie.id).subscribe(trailers => {
+      //     console.log(`${movie.title} trailer:`, trailers);
+      //   });
+      // }
+    });
+  }
+
+  onMovieClick(movieId: string) {
+    // Handle the click event. For example, navigate to a detail page or open a modal to show the trailer.
+    // You can use Angular Router here if you want to navigate to a detail component
+    this.movieService.getMovieTrailers(movieId).subscribe(trailers => {
+      console.log(`${movieId} trailer:`, trailers);
     });
   }
 }
--- libs/xplat/core/src/lib/services/base/movie.base-service.ts
+++ libs/xplat/core/src/lib/services/base/movie.base-service.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 export abstract class MovieBaseService {
   private apiUrl = 'https://api.themoviedb.org/3';
@@ -8,5 +9,19 @@ export abstract class MovieBaseService {
 
   getUpcomingMovies() {
     return this.http.get(`${this.apiUrl}/movie/upcoming?api_key=${this.apiKey}&language=en-US`);
+  }
+
+  getMovieTrailers(movieId: string) {
+    const url = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${this.apiKey}`;
+    return this.http.get(url).pipe(
+      map((response: any) => {
+        // Filter for YouTube trailers
+        // @ts-ignore
+        const trailers = response.results.filter(video => video.site === 'YouTube' && video.type === 'Trailer');
+        // Map to full YouTube URLs
+        // @ts-ignore
+        return trailers.map(trailer => `https://www.youtube.com/watch?v=${trailer.key}`);
+      })
+    );
   }
 }
